fa817a96d6c033ab75cc8a1ecdecfa57126960f7
DariussCloud <82780562+DariussCloud@users.noreply.github.com>
2021-05-11 23:46:19 -0500

    Add files via upload

--- PythonEjercicio/PythonEjercicio.py
+++ PythonEjercicio/PythonEjercicio.py
@@ -3,6 +3,7 @@ import sqlite3
 import time
 import locale
 import pytz
+import string
 from tkinter import *
 from tkinter import messagebox
 
@@ -15,7 +16,19 @@ n1 = StringVar()
 n2 = StringVar()
 r = StringVar()
 signos = ["/", "*", "-", "+"]
-sig = StringVar()
+relog_ = StringVar()
+locale.setlocale(locale.LC_ALL, 'es-MX')
+dt = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
+fecha = dt.strftime("%A %d %B %Y")
+hora =  dt.strftime("%I:%M:%S")
+
+
+
+
+
+
+
+
 def calculadora():
     
 
@@ -26,31 +39,66 @@ def calculadora():
     entryUsr.grid_remove()
     entryPass.grid_remove()
     acc.grid_remove()
+    sal.grid_remove()
     
+    
+    global numero1
+    global entryNum
+    global numero2
+    global entryNum2
+    global resultado
+    global resultado2
+    global mas
+    global menos
+    global por
+    global entre
+    global Vlogs
+    global salr
+    global back
 
-    Label(root, text="\nNumero 1",font=("Arial Bold", 10)).grid(row=3, column=0, padx=5, pady=5)
-    Entry(root, justify=CENTER, textvariable=n1).grid(row=4, column=0, padx=5, pady=5)
-
-    Label(root, justify=CENTER, textvariable = sig).grid(row=4, column=1, padx=5, pady=5, ipadx=5, sticky="w")
+    numero1 = Label(root, text="\nNumero 1       ",font=("Arial Bold", 10))
+    numero1.grid(row=3, column=0, padx=5, pady=5, sticky="se")
+    
+    entryNum = Entry(root, justify=CENTER, textvariable=n1)
+    entryNum.grid(row=4, column=0, padx=5, pady=5, sticky="ne"  )
     
     
-    Label(root, text="\n           Numero 2",font=("Arial Bold", 10)).grid(row=3, column=1, padx=5, pady=5)
-    Entry(root, justify=CENTER, textvariable=n2).grid(row=4, column=1, padx=5, pady=5, sticky="e")
-
-    Label(root, text="\nResultado",font=("Arial Bold", 10)).grid(row=3, column=2, padx=5, pady=5)
-    Entry(root, justify=CENTER, state=DISABLED, textvariable=r).grid(row=4, column=2, padx=5, pady=5, sticky="e")
-
     
-    Button(root, text="+", command=sumar,font=("Arial Bold", 10)).grid(row=3,column=3, padx=5, pady=5, sticky="w", ipadx=8, ipady=5)
-    Button(root, text="-", command=restar,font=("Arial Bold", 10)).grid(row=3,column=4, padx=5, pady=5, sticky="e", ipadx=8, ipady=5)
-    Button(root, text="*", command=multiplicar,font=("Arial Bold", 10)).grid(row=4,column=3, padx=5, pady=5, sticky="w", ipadx=8, ipady=5)
-    Button(root, text="/", command=dividir,font=("Arial Bold", 10)).grid(row=4,column=4, padx=5, pady=5, sticky="e", ipadx=8, ipady=5)
-    Button(root, text="Ver los logs", command=logs,font=("Arial Bold", 10)).grid(row=3,column=5, padx=5, pady=5, sticky="w")
-    Button(root, text="Salir", command=salir,font=("Arial Bold", 10)).grid(row=4,column=5, padx=5, pady=5, sticky="w")
-    Button(root, text="⪻", command=remove,font=("Arial Bold", 10)).grid(row=4,column=5, padx=5, pady=5, sticky="e")
+    numero2 = Label(root, text="\nNumero 2",font=("Arial Bold", 10))
+    numero2.grid(row=3, column=1, padx=5, pady=5, sticky="s")
+    
+    entryNum2 = Entry(root, justify=CENTER, textvariable=n2)
+    entryNum2.grid(row=4, column=1, padx=5, pady=5, sticky="n")
     
     
-
+    
+    resultado = Label(root, text="\nResultado",font=("Arial Bold", 10))
+    resultado.grid(row=3, column=2, padx=5, pady=5, sticky="s")
+    
+    resultado2 = Entry(root, justify=CENTER, state=DISABLED, textvariable=r)
+    resultado2.grid(row=4, column=2, padx=5, pady=5, sticky="n")
+    
+    mas = Button(root, text="+", command=sumar,font=("Arial Bold", 10))
+    mas.grid(row=3,column=3, padx=5, pady=5, sticky="w", ipadx=8, ipady=5)
+    
+    menos = Button(root, text="-", command=restar,font=("Arial Bold", 10))
+    menos.grid(row=3,column=4, padx=5, pady=5, sticky="e", ipadx=8, ipady=5)
+    
+    por = Button(root, text="*", command=multiplicar,font=("Arial Bold", 10))
+    por.grid(row=4,column=3, padx=5, pady=5, sticky="w", ipadx=8, ipady=5)
+    
+    entre = Button(root, text="/", command=dividir,font=("Arial Bold", 10))
+    entre.grid(row=4,column=4, padx=5, pady=5, sticky="e", ipadx=8, ipady=5)
+    
+    Vlogs = Button(root, text="Ver los logs", command=logs,font=("Arial Bold", 10))
+    Vlogs.grid(row=3,column=5, padx=5, pady=5, sticky="w")
+    
+    salr = Button(root, text="Salir", command=salir,font=("Arial Bold", 10))
+    salr.grid(row=4,column=5, padx=5, pady=5, sticky="w")
+    
+    back = Button(root, text="⪻", command=remove,font=("Arial Bold", 10))
+    back.grid(row=4,column=5, padx=5, pady=5, sticky="e")
+    
 
 def convertirTupla(tup):
     str =  ''.join(tup)
@@ -79,7 +127,7 @@ def crear():
 
 def salir():
     root.destroy()
-
+    
 def acceder():
     
     
@@ -106,78 +154,83 @@ def borrar():
     n2.set('')
     
 def sumar():
-    r.set(float( n1.get() ) + float(n2.get() ) )
-    sig.set("+")
-    global operacion
-    operacion = signos[3]
-    commit()
-    borrar()
-    
-def restar():
-    r.set( float( n1.get() ) - float(n2.get() ) )
-    sig.set("-")
-    global operacion
-    operacion = signos[2]
-    commit()
-    borrar()
-    
-def multiplicar():
-    r.set( float( n1.get() ) * float(n2.get() ) )
-    sig.set("*")
-    global operacion
-    operacion = signos[1]
-    commit()
-    borrar()
-def dividir():
-    r.set( float( n1.get() ) / float(n2.get() ) )
-    sig.set("*")
-    global operacion
-    operacion = signos[0]
-    
-    commit()
-    borrar()
+    try:
+        r.set(float( n1.get() ) + float(n2.get() ) )
 
+        global operacion
+        operacion = signos[3]
+        commit()
+        borrar()
+    except ValueError:
+        texto.set("Error-\ningresa los campos correctamente")
+        
+        
+        
+def restar():
+    try:
+        
+        r.set( float( n1.get() ) - float(n2.get() ) )
+
+        global operacion
+        operacion = signos[2]
+        commit()
+        borrar()
+    except ValueError:
+        texto.set("Error-\ningresa los campos correctamente")
+        
+        
+        
+def multiplicar():
+    try:
+        r.set( float( n1.get() ) * float(n2.get() ) )
+
+        global operacion
+        operacion = signos[1]
+        
+        commit()
+        borrar()
+        
+    except ValueError:
+        texto.set("Error-\ningresa los campos correctamente")
+        
+        
+        
+        
+def dividir():
+    try:
+        r.set( float( n1.get() ) / float(n2.get() ) )
+        global operacion
+        operacion = signos[0]
+        
+        commit()
+        borrar()
+    
+    except ValueError:
+        texto.set("Error-\ningresa los campos correctamente")
+        
+        
 def logs():
     conexion = sqlite3.connect("PythonEjercicio.db")
     cursor = conexion.cursor()
     
-    
+    global asd
     
     for log in cursor.execute('''SELECT * FROM logs''').fetchall():
        
-        """
-       asd = ""
-        a = log[0],log[1],log[2]," = ",log[3]," Por ",log[4]," A las ",log[5]," Hora india"
-        strtexto = convertirTupla(a)
-        asd += strtexto
-        Label(root, text=asd).grid()
-        """
     
-    asd = ""
-    for log in cursor.execute('''SELECT * FROM logs''').fetchall():
-        a = log[0],log[1],log[2],"=",log[3]," Por ",log[4]," A las ",log[5]," Hora india\n"
+       asd = ""
+       a = log[0] + log[1] + log[2] + " = " + log[3] + " Hecho por " + log[4] + " El dia " + log[5] + " A las " + log[6]
+       strtexto = convertirTupla(a)
+       asd += strtexto
+       loglabel = Label(root, text=asd, font=("Arial Bold", 10))
+       loglabel.config()
+       loglabel.grid()        
 
-        strtexto = convertirTupla(a)
-        print(strtexto)
-        asd += strtexto
-
-    messagebox.showinfo("Logs", asd)
-
-      
-        
-        
-        
-
-        
-        
         
     conexion.commit
     conexion.close()
-
-
-
-
-
+  
+      
 def commit():
     conexion = sqlite3.connect("PythonEjercicio.db")
     cursor = conexion.cursor()
@@ -185,23 +238,22 @@ def commit():
 
 
 
-    locale.setlocale(locale.LC_ALL, 'es-MX')
-    dt = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-    hora = dt.strftime("%A %d %B %Y %I:%M:%S")
+    
     resultado = r.get()
     num1 = str(n1.get())
     num2 = str(n2.get())
     Usr = str(entryUsr.get())
     
 
-    cursor.execute('INSERT INTO logs (numero1, numero2, operacion, resultado, usuario, Hora) VALUES (?,?,?,?,?,?)',
+    cursor.execute('INSERT INTO logs (numero1, numero2, operacion, resultado, usuario, fecha, Hora) VALUES (?,?,?,?,?,?,?)',
         [
         num1,
         num2,
         operacion,
         resultado,
         Usr,
-        hora,
+        fecha,
+        hora
         ])
     
 
@@ -214,14 +266,42 @@ def commit():
 root.title("PythonEjercicio")
 root.iconbitmap('linux.ico')
 root.config(cursor="plus",relief="ridge",bd=15)
-root.geometry('650x780')
 root.resizable(0,0)
 
 
+
+def time():
+    
+    
+    clock = Label(root,font=("Arial Bold", 10))
+    clock.grid(row=0,column=5,pady=5,padx=5)
+
+    dt = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
+    hora =  dt.strftime("%I:%M:%S")
+    clock.config(text=hora)
+    clock.after(200,time)
+
+
+
+
+
 def remove():
-    pass
+    numero1.grid_remove()
+    entryNum.grid_remove()
+    numero2.grid_remove()
+    entryNum2.grid_remove()
+    resultado.grid_remove()
+    resultado2.grid_remove()
+    mas.grid_remove()
+    menos.grid_remove()
+    por.grid_remove()
+    entre.grid_remove()
+    Vlogs.grid_remove()
+    salr.grid_remove()
+    back.grid_remove()
+    texto.set("")
     
-    
+    inicio()
     
 def inicio():
 
@@ -230,39 +310,39 @@ def inicio():
 
     global mesg
     mesg = StringVar()
-    mesg.set("Iniciar sesion")
+    mesg.set("Iniciar  sesion")
     global msg
     msg = Label(root,font=("Arial Bold", 15))
-    msg.grid(column=0)
-    msg.config(textvariable=mesg, relief="ridge", justify="left")
+    msg.grid(row=0)
+    msg.config(textvariable=mesg, relief="ridge", justify="center")
 
 
     global labelUsr
     labelUsr = Label(root, text="Usuario:",font=("Arial Bold", 10))
-    labelUsr.grid(row=1, column=0, padx=50, pady=5)
+    labelUsr.grid(row=1, padx=50, pady=5)
     labelUsr.config(justify="center")
 
     global entryUsr
     entryUsr = Entry(root)
-    entryUsr.grid(row=2, column=0, padx=5, pady=5)
+    entryUsr.grid(row=2, padx=5, pady=5)
     entryUsr.config(justify="center", state="normal")
 
 
     global labelPass
     labelPass = Label(root, text="Contraseña:",font=("Arial Bold", 10))
-    labelPass.grid(row=3, column=0, padx=5, pady=5)
+    labelPass.grid(row=3, padx=5, pady=5)
     
     
     global entryPass
     entryPass = Entry(root)
-    entryPass.grid(row=4, column=0, padx=5, pady=5)
+    entryPass.grid(row=4, padx=5, pady=5)
     entryPass.config(justify="center", show="*")
 
     global texto
     texto = StringVar()
     texto.set("")
     label = Label(root, text="Usuario:",font=("Arial Bold", 10))
-    label.grid(row=0, column=1, padx=5, pady=5)
+    label.grid(column=1, row=0, padx=5, pady=5)
     label.config(textvariable=texto )
 
 
@@ -272,14 +352,23 @@ def inicio():
 
 
     global acc
-    acc = Button(root, textvariable=button_acceder, command=acceder)
-    acc.grid(row=5, column=0, padx=5, pady=5)
+    acc = Button(root, textvariable=button_acceder, command=acceder,font=("Arial Bold", 10))
+    acc.grid(row=5, padx=5, pady=5)
 
     global crear
-    crear = Button(root, text="Crear cuenta", command=crear)
-    crear.grid(row=6, column=0, padx=5, pady=5)
+    crear = Button(root, text="Crear cuenta", command=crear,font=("Arial Bold", 10))
+    crear.grid(row=6, padx=5, pady=5)
+    global sal
+    sal = Button(root, text="Salir", command=salir,font=("Arial Bold", 10))
+    sal.grid(row=7, padx=5, pady=5)
+    
+    
+    
+    
+time()  
 inicio()
 root.mainloop()
+
 
 
 
